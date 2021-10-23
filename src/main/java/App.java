1bf3a2d5397a304e62fe5f3161a31b714260c34f
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-23 23:35:33 +0300

    add branching to the app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,47 +1,56 @@
 import modules.Encode;
 
 import java.io.IOException;
-import java.util.Scanner;
 import  java.io.BufferedReader;
 import  java.io.InputStreamReader;
 
 public class App{
     public static void main(String[] args) throws IOException {
-        Scanner myScanner = new Scanner(System.in);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        //input string from user
-        System.out.println("Enter your  words to encrypt:");
-        String theInputString = myScanner.nextLine();
+        boolean appRunning = true;
+        while (appRunning){
+            //input string from user
+            System.out.println("Enter your word(s) to encrypt:");
+            String theInputString = bufferedReader.readLine();
 
-        Encode stringUserInput = new Encode();
-        String words = stringUserInput.isInputString(theInputString);
-        System.out.println("This is your word to encrypt : "+words);
+            Encode stringUserInput = new Encode();
+            String words = stringUserInput.isInputString(theInputString);
+            System.out.println("This is your word to encrypt : "+words);
 
-        System.out.println("Enter your key (any number : 1-25)");
-        Integer theInputInteger = myScanner.nextInt();
+            System.out.println("Enter your key (any number : 1-25)");
+            String theInputInteger = bufferedReader.readLine();
+            Integer newInputInteger = Integer.parseInt(theInputInteger);
 
-        Encode inputInteger = new Encode();
-        Integer key = inputInteger.isInputInteger(theInputInteger);
+            Encode inputInteger = new Encode();
+            Integer key = inputInteger.isInputInteger(newInputInteger);
 
-        System.out.println("Your word(s) will be "+ key + " letter(s) ahead!!");
+            System.out.println("Your word(s) will be "+ key + " position(s) ahead of the alphabetical order!!");
 
-        Encode yourEncryption = new Encode();
-        String encrypt = yourEncryption.encodeYourWord(words,key);
+            Encode yourEncryption = new Encode();
+            String encrypt = yourEncryption.encodeYourWord(words,key);
 
-        System.out.println("This is your encrypt : "+encrypt);
+            System.out.println("This is your encrypt : "+encrypt);
 
-        System.out.println("Do you want to decrypt your word(s) ? \n Enter Decrypt");
-        String choice = bufferedReader.readLine();
+            System.out.println("Do you want to decrypt your word(s) ? \n Enter Decrypt or Exit");
+            String choice = bufferedReader.readLine();
 
-        if(choice.equals("Decrypt")) {
-            Decode yourDecrypt = new Decode();
-            String decrypt = yourDecrypt.decodeEncryption(encrypt,key);
+            if(choice.equals("Decrypt")) {
+                Decode yourDecrypt = new Decode();
+                String decrypt = yourDecrypt.decodeEncryption(encrypt,key);
 
-            System.out.println("Input String :" + words);
-            System.out.println("Encrypted String :" + encrypt);
-            System.out.println("Decrypted String :" + decrypt);
+                System.out.println("Input String :" + words);
+                System.out.println("Encrypted String :" + encrypt);
+                System.out.println("Decrypted String :" + decrypt);
+            }
+            else if (choice.equals("Exit")){
+                appRunning =false;
+            }
+            else {
+                System.out.println("Sorry!! that input is not applicable.");
+            }
         }
+
 
     }
 }
