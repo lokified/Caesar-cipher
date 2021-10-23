6ee05f17b9a20564cbcc69e26e0777e8561ab03b
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-23 22:25:15 +0300

    add a function that returns a decrypted string

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,9 +1,32 @@
+import modules.Encode;
 public class Decode {
 
     private String yourWord;
-    private int yourKey;
+    private String yourDecrypt;
 
     public String isInputString(String word) {
         return this.yourWord = word;
+    }
+
+    public String decodeEncryption(String word, int key){
+
+        String decrypt = "";
+
+        for ( int i = 0; i <word.length(); i++  ) {
+
+            //shift character
+            char letter = word.charAt(i);
+
+            if (letter >= 'a' && letter <= 'z') {
+                letter = (char) (letter - key);
+
+                //loop back to start
+                if(letter < 'a') {
+                    letter = (char) (letter - 'a'+'z'+1);
+                }
+            }
+            decrypt += letter;
+        }
+        return this.yourDecrypt = decrypt;
     }
 }
